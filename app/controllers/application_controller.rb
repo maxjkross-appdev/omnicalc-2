c8867c6e42a9e06956ba4979827850954ce8253f
maxjkross <maxjkross@uchicago.edu>
2023-02-01 22:11:23 +0000

    add functionality finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ :template => "calculation_templates/add_form.html.erb" })
+  end
 
+  def wizard_add
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first + @second
+    render({ :template => "calculation_templates/wizard_add_form.html.erb" })
+  end
 end
