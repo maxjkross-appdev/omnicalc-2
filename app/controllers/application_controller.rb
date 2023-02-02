926c58c450a7ed2931a69a13551329b43ea785a3
maxjkross <maxjkross@uchicago.edu>
2023-02-02 04:04:25 +0000

    final submission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,37 @@ class ApplicationController < ActionController::Base
     @result = @first + @second
     render({ :template => "calculation_templates/wizard_add_form.html.erb" })
   end
+
+  def subtract
+    render({ :template => "calculation_templates/subtract_form.html.erb" })
+  end
+
+  def wizard_subtract
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first - @second
+    render({ :template => "calculation_templates/wizard_subtract_form.html.erb" })
+  end
+
+  def multiply
+    render({ :template => "calculation_templates/multiply_form.html.erb" })
+  end
+
+  def wizard_multiply
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first * @second
+    render({ :template => "calculation_templates/wizard_multiply_form.html.erb" })
+  end
+
+  def divide
+    render({ :template => "calculation_templates/divide_form.html.erb" })
+  end
+
+  def wizard_divide
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first / @second
+    render({ :template => "calculation_templates/wizard_divide_form.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ get("/add", {:controller => "application", :action => "add"})
 
 get("/wizard_add", {:controller => "application", :action => "wizard_add"})
 
+get("/subtract", {:controller => "application", :action => "subtract"})
+
+get("/wizard_subtract", {:controller => "application", :action => "wizard_subtract"})
+
+get("/multiply", {:controller => "application", :action => "multiply"})
+
+get("/wizard_multiply", {:controller => "application", :action => "wizard_multiply"})
+
+get("/divide", {:controller => "application", :action => "divide"})
+
+get("/wizard_divide", {:controller => "application", :action => "wizard_divide"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
